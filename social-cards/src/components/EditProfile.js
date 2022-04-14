608d09d92df244995e84cca9c62c6686e1f21078
Paul Reece <paulreece2@gmail.com>
2022-04-14 15:42:59 -0400

    one more

--- social-cards/src/components/EditProfile.js
+++ social-cards/src/components/EditProfile.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "../App.css";
-import { Link } from "react-router-dom";
 import Select from "react-select";
 
 export const EditProfile = ({ setAuth, token, username, profilePk }) => {
